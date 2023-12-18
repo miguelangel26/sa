5856275cefb77085999661e156d55fec0d580ce8
miguel angel <74755907+miguelangel26@users.noreply.github.com>
2023-12-18 18:56:17 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById("loginButton").addEventListener("click", function() {
+    // Obtiene los valores del nombre de usuario y la contraseña
+    var username = document.querySelector("input[placeholder='Username']").value;
+    var password = document.querySelector("input[placeholder='Password']").value;
+  
+    // Verifica si las credenciales son correctas (puedes cambiar estos valores según tus necesidades)
+    if (username === "usuario" && password === "contrasena") {
+      // Credenciales correctas, redirige a tarea.html
+      window.location.href = "tarea.html";
+    } else {
+      // Credenciales incorrectas, muestra un mensaje de error
+      alert("Usuario o contraseña incorrectos. Por favor, inténtalo de nuevo.");
+      // También podrías mostrar un mensaje en el HTML en lugar de usar un alert
+    }
+  });
+  
---
+++ tarea.js
@@ -0,0 +1,68 @@
+function Tarea() {
+    var agregarTarea = document.getElementById("agregarTarea");
+    var agregarLista = document.getElementById("agregarLista");
+
+    if (agregarTarea.value.trim() === "") {
+        alert("Por favor, ingrese una tarea.");
+        return;
+    }
+
+    var crearTarea = document.createElement("div");
+    crearTarea.className = "tarea";
+
+    crearTarea.innerHTML = `
+        <span>${agregarTarea.value}</span>
+        <button onclick="CompletarTarea(this)">Completar</button>
+    `;
+
+    agregarLista.appendChild(crearTarea);
+    agregarTarea.value = "";
+
+    // Verificar si todas las tareas están completas
+    var todasLasTareasCompletas = verificarTodasLasTareasCompletas();
+
+    // Habilitar o deshabilitar el botón según el estado de las tareas
+    var iniciarOtroDiaBtn = document.getElementById("iniciarOtroDiaBtn");
+    iniciarOtroDiaBtn.disabled = !todasLasTareasCompletas;
+}
+
+function CompletarTarea(button) {
+    var crearTarea = button.parentElement;
+    crearTarea.classList.toggle("tareaCompleta");
+
+    // Verificar si todas las tareas están completas
+    var todasLasTareasCompletas = verificarTodasLasTareasCompletas();
+
+    // Habilitar o deshabilitar el botón según el estado de las tareas
+    var iniciarOtroDiaBtn = document.getElementById("iniciarOtroDiaBtn");
+    iniciarOtroDiaBtn.disabled = !todasLasTareasCompletas;
+
+    // Aplicar la clase "botonCompletar" al botón
+    button.classList.toggle("botonCompletar");
+}
+
+function verificarTodasLasTareasCompletas() {
+    var tareas = document.getElementsByClassName("tarea");
+    for (var i = 0; i < tareas.length; i++) {
+        if (!tareas[i].classList.contains("tareaCompleta")) {
+            return false; // Al menos una tarea no está completa
+        }
+    }
+    return true; // Todas las tareas están completas
+}
+
+function IniciarOtroDia() {
+    var agregarLista = document.getElementById("agregarLista");
+    agregarLista.innerHTML = ""; // Elimina todas las tareas existentes
+
+    // Deshabilitar el botón después de reiniciar
+    var iniciarOtroDiaBtn = document.getElementById("iniciarOtroDiaBtn");
+    iniciarOtroDiaBtn.disabled = true;
+}
+
+
+
+
+
+
+
